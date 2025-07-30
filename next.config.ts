e8127fd5ac3c069bbc169ab00b53ca4904558f35
veer <veer123@gmail.com>
2025-07-30 14:25:39 +0530

    changes

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,36 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "api.pvclasses.in",
+//         pathname: "/upload/**",
+//       },
+
+//       //   {
+//       //   protocol: "http",
+//       //   hostname: "localhost",
+//       //   port: "3000", // optional, but good for clarity
+//       //   pathname: "/upload/**",
+//       // },
+//       {
+//         protocol: "https",
+//         hostname: "gratisography.com",
+//         pathname: "/**",
+//       },
+//     ],
+//   },
+// };
+
+// module.exports = nextConfig;
+
+
+
+
+
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -7,13 +40,12 @@ const nextConfig = {
         hostname: "api.pvclasses.in",
         pathname: "/upload/**",
       },
-
-      //   {
-      //   protocol: "http",
-      //   hostname: "localhost",
-      //   port: "3000", // optional, but good for clarity
-      //   pathname: "/upload/**",
-      // },
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "5000",
+        pathname: "/upload/**",
+      }, // ✅ Add comma here
       {
         protocol: "https",
         hostname: "gratisography.com",
